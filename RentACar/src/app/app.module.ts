47836275d729eb5224c42ce4ff400336558e89a2
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-04-04 12:43:26 +0300

    Initial commit

---
+++ RentACar/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CarComponent } from './components/car/car.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { ColorComponent } from './components/color/color.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { RentalComponent } from './components/rental/rental.component';
+import { CardetailsComponent } from './components/cardetails/cardetails.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarComponent,
+    BrandComponent,
+    ColorComponent,
+    CustomerComponent,
+    NaviComponent,
+    RentalComponent,
+    CardetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ RentACar/src/app/components/cardetails/cardetails.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cardetails',
+  templateUrl: './cardetails.component.html',
+  styleUrls: ['./cardetails.component.css']
+})
+export class CardetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
