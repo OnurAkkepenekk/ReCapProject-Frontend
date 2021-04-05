2d3ee1a959b46f6599932fac19d4755cecf4a093
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-04-05 23:30:48 +0300

    Update brand component

--- RentACar/src/app/components/brand/brand.component.ts
+++ RentACar/src/app/components/brand/brand.component.ts
@@ -8,6 +8,7 @@ import { BrandService } from 'src/app/services/brand.service';
   styleUrls: ['./brand.component.css'],
 })
 export class BrandComponent implements OnInit {
+  currentBrand: Brand;
   brands: Brand[] = [];
   dataLoaded = false;
   constructor(private brandService: BrandService) {}
@@ -21,5 +22,15 @@ export class BrandComponent implements OnInit {
       console.log(response);
       this.dataLoaded = true;
     });
+  }
+  setCurrentBrand(brand: Brand) {
+    this.currentBrand = brand;
+  }
+  getCurrentBrandClass(brand: Brand) {
+    if (brand == this.currentBrand) {
+      return 'list-group-item active';
+    } else {
+      return 'list-group-item';
+    }
   }
 }
