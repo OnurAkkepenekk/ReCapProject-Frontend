11bd42172fc69839c563720306548192d85dfd47
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-04-05 23:28:59 +0300

    Update car.service.ts

--- RentACar/src/app/services/car.service.ts
+++ RentACar/src/app/services/car.service.ts
@@ -1,18 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Brand } from '../models/brand';
 import { Car } from '../models/car';
 import { ListResponseModel } from '../models/listResponseModel';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CarService {
+  constructor(private httpClient: HttpClient) {}
+  apiUrl = 'https://localhost:44369/api/';
 
-  constructor(private httpClient:HttpClient) { }
-  apiUrl = 'https://localhost:44369/api/cars/getall';
+  getCars(): Observable<ListResponseModel<Car>> {
+    let newPath = this.apiUrl + 'cars/getall';
+    return this.httpClient.get<ListResponseModel<Car>>(newPath);
+  }
 
-  getCars():Observable<ListResponseModel<Car>>{
-    return this.httpClient.get<ListResponseModel<Car>>(this.apiUrl)
+  getCarsByBrand(brandId: number): Observable<ListResponseModel<Car>> {
+    let newPath = this.apiUrl + 'cars/GetByBrandId?brandId=' + brandId;
+    console.log(newPath);
+    return this.httpClient.get<ListResponseModel<Car>>(newPath);
+  }
+  getCarsByColor(colorId: number): Observable<ListResponseModel<Car>> {
+    let newPath = this.apiUrl + 'cars/GetByColorId?colorId=' + colorId;
+    console.log(newPath);
+    return this.httpClient.get<ListResponseModel<Car>>(newPath);
   }
 }
