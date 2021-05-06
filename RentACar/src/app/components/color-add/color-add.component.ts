5164c966b25147ba1e6056942be8629a775a10ea
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-05-06 21:42:18 +0300

    Add color-add component

--- RentACar/src/app/components/color-add/color-add.component.ts
+++ RentACar/src/app/components/color-add/color-add.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Color } from 'src/app/models/color';
+import { ColorService } from 'src/app/services/color.service';
 
 @Component({
   selector: 'app-color-add',
   templateUrl: './color-add.component.html',
-  styleUrls: ['./color-add.component.css']
+  styleUrls: ['./color-add.component.css'],
 })
 export class ColorAddComponent implements OnInit {
-
-  constructor() { }
+  colors: Color[] = [];
+  colorAddForm: FormGroup;
+  constructor(
+    private colorService: ColorService,
+    private formBuilder: FormBuilder,
+    private toasterService: ToastrService
+  ) {}
 
   ngOnInit(): void {
+    this.createAddColorFrom();
   }
 
+  createAddColorFrom() {
+    this.colorAddForm = this.formBuilder.group({
+      colorName: ['', Validators.required],
+    });
+  }
+  add() {
+    let colorModel = Object.assign({}, this.colorAddForm.value);
+    if (this.colorAddForm.valid) {
+      this.colorService.add(colorModel).subscribe((response) => {
+        if (response.success) {
+          this.toasterService.success('Color added', 'Success');
+        }
+      });
+    } else {
+      this.toasterService.error("Color didn't add", 'Error');
+    }
+  }
 }
--- RentACar/src/app/services/color.service.ts
+++ RentACar/src/app/services/color.service.ts
@@ -3,14 +3,21 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Color } from '../models/color';
 import { ListResponseModel } from '../models/listResponseModel';
+import { SingleResponseModel } from '../models/singleResponseModel';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ColorService {
-  apiUrl = 'https://localhost:44369/api/colors/getall';
+  apiUrl = 'https://localhost:44369/api/';
   constructor(private httpClient: HttpClient) {}
-  getColors():Observable<ListResponseModel<Color>>{
-    return this.httpClient.get<ListResponseModel<Color>>(this.apiUrl);
+  getColors(): Observable<ListResponseModel<Color>> {
+    return this.httpClient.get<ListResponseModel<Color>>(
+      this.apiUrl + 'colors/getall'
+    );
+  }
+
+  add(color: Color) {
+    return this.httpClient.post<SingleResponseModel<Color>>(this.apiUrl + "colors/add", color);
   }
 }
