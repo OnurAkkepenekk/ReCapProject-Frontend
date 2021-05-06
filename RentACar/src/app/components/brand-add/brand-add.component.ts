729f613bb5cef8637c0e02073ffda2e0c22bf5ef
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-05-06 21:41:57 +0300

    Add brand-add

--- RentACar/src/app/components/brand-add/brand-add.component.ts
+++ RentACar/src/app/components/brand-add/brand-add.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Brand } from 'src/app/models/brand';
+import { BrandService } from 'src/app/services/brand.service';
 
 @Component({
   selector: 'app-brand-add',
   templateUrl: './brand-add.component.html',
-  styleUrls: ['./brand-add.component.css']
+  styleUrls: ['./brand-add.component.css'],
 })
 export class BrandAddComponent implements OnInit {
+  brands: Brand[] = [];
+  brandAddForm: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private brandService: BrandService,
+    private toasterService: ToastrService
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  createAddBrandForm() {
+    this.brandAddForm = this.formBuilder.group({
+      brandName: ['', Validators.required],
+    });
   }
+  ngOnInit(): void {
+    this.createAddBrandForm();
+  }
+  add() {
+    let brandModel = Object.assign({}, this.brandAddForm.value);
 
+    if (this.brandAddForm.valid) {
+      this.brandService.addBrand(brandModel).subscribe((response) => {
+        if (response.success) {
+          this.toasterService.success('Brand added successfully', 'Success');
+        } else {
+          this.toasterService.success("Brand didn't add", 'Error');
+        }
+      });
+    }
+  }
 }
--- RentACar/src/app/services/brand.service.ts
+++ RentACar/src/app/services/brand.service.ts
@@ -3,14 +3,21 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Brand } from '../models/brand';
 import { ListResponseModel } from '../models/listResponseModel';
+import { SingleResponseModel } from '../models/singleResponseModel';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BrandService {
-  apiUrl = 'https://localhost:44369/api/brands/getall';
+  apiUrl = 'https://localhost:44369/api/';
   constructor(private httpClient: HttpClient) {}
+
   getBrands(): Observable<ListResponseModel<Brand>> {
-    return this.httpClient.get<ListResponseModel<Brand>>(this.apiUrl);
+    return this.httpClient.get<ListResponseModel<Brand>>(
+      this.apiUrl + 'brands/getall'
+    );
+  }
+  addBrand(brand: Brand) {
+    return this.httpClient.post<SingleResponseModel<Brand>>(this.apiUrl + 'brands/add', brand);
   }
 }
