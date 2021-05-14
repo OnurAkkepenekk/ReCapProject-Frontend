a6505644a012aecd264e47f6e238c76064bdb8c1
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-05-14 03:42:03 +0300

    Update app-routing.module.ts

--- RentACar/src/app/app-routing.module.ts
+++ RentACar/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BrandAddComponent } from './components/brand-add/brand-add.component';
+import { BrandUpdateComponent } from './components/brand-update/brand-update.component';
 import { CarAddComponent } from './components/car-add/car-add.component';
 import { CarDetailsComponent } from './components/car-details/car-details.component';
+import { CarUpdateComponent } from './components/car-update/car-update.component';
 import { CarComponent } from './components/car/car.component';
 import { ColorAddComponent } from './components/color-add/color-add.component';
+import { ColorUpdateComponent } from './components/color-update/color-update.component';
 import { PaymentComponent } from './components/payment/payment.component';
 import { RentalComponent } from './components/rental/rental.component';
 
@@ -39,17 +42,37 @@ const routes: Routes = [
     component: RentalComponent,
   },
   {
-    path: 'rentals/:carId', component: PaymentComponent
+    path: 'rentals/:carId',
+    component: PaymentComponent,
+  },
+
+  
+  {
+    path: 'cars/add',
+    component: CarAddComponent,
   },
   {
-    path: 'cars/add', component: CarAddComponent
+    path: 'brands/add',
+    component: BrandAddComponent,
   },
   {
-    path: 'brands/add', component: BrandAddComponent
+    path: 'colors/add',
+    component: ColorAddComponent,
+  },
+
+
+  {
+    path: 'cars/update/:carId',
+    component: CarUpdateComponent,
   },
   {
-    path: 'colors/add', component: ColorAddComponent
-  }
+    path: 'colors/update/:id',
+    component: ColorUpdateComponent,
+  },
+  {
+    path: 'brands/update/:brandId',
+    component: BrandUpdateComponent,
+  },
 ];
 
 @NgModule({
