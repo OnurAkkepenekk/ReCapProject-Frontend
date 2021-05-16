6ee2398b28456f3d4ac162b39525685d455e5c6d
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-05-16 16:49:26 +0300

    Create login component

---
+++ RentACar/src/app/components/login/login.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private toastrService: ToastrService
+  ) {}
+
+  ngOnInit(): void {
+    this.createLoginForm();
+  }
+
+  createLoginForm() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+      let loginModel = Object.assign({}, this.loginForm.value);
+      this.authService.login(loginModel).subscribe(
+        (response) => {
+          this.toastrService.info(response.message);
+          localStorage.setItem('token', response.data.token);
+          console.log(response);
+        },
+        (responseError) => {
+          this.toastrService.error(responseError.error);
+        }
+      );
+    }
+  }
+}
